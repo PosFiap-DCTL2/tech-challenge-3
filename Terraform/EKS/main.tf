13cac6890f981d2e591a8e03e444be081da43ecb
DaniloRferraz <danilor.ferraz@outlook.com>
2026-04-14 11:40:18 -0300

    Adicionado as variaveis do EKS e configurado o EKS

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -1,3 +1,5 @@
+### Cluster EKS ###
+
 resource "aws_eks_cluster" "clusterpos" {
   name = "clusterpos"
 
@@ -6,17 +8,57 @@ resource "aws_eks_cluster" "clusterpos" {
   }
   
   bootstrap_self_managed_addons = true
-  role_arn = arn:aws:iam::123945314948:role/LabRole
-  version  = "1.31"
+  role_arn = "arn:aws:iam::123945314948:role/LabRole"
+  version  = "1.30"
+
+  compute_config {
+    enabled = true
+    node_pools = ["general-purpose"]
+    node_role_arn = arn:aws:iam::123945314948:role/LabRole
+
+  }
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.az1.id,
-      aws_subnet.az2.id,
-      aws_subnet.az3.id,
+        var.subnet_ids[0],
+        var.subnet_ids[1],  
+        var.subnet_ids[2],
+        var.subnet_ids[3],
     ]
   }
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKS_CNI_Policy,
   ]
+}
+
+### Node Group EKS ###
+
+resource "aws_eks_node_group" "nodegrouppos" {
+  cluster_name    = aws_eks_cluster.clusterpos.name
+  node_group_name = "nodegrouppos"
+  node_role_arn   = "arn:aws:iam::123945314948:role/LabRole"
+  subnet_ids      = var.subnet_ids
+
+  capacity_type   = "ON_DEMAND"
+  instance_types  = var.instance_types
+  
+  force_update_version = true
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 4
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+depends_on = [
+  aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+  aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+  aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+]
 }
--- Terraform/EKS/variable.tf
+++ Terraform/EKS/variable.tf
@@ -1,3 +1,16 @@
-variable "" {
-  
+variable "subnet_ids" {
+  description = "IDs das subnets usadas pelo EKS"
+  type        = list(string)
+  default     = [
+    "aws_subnet.subnetpublica1.id",
+    "aws_subnet.subnetpublica2.id",
+    "aws_subnet.subnetprivada1.id",
+    "aws_subnet.subnetprivada2.id"
+  ]
+}
+
+variable "instance_types" {
+  description = "Variavel para troca de tipo de instanca"
+  type = string
+  default = "t3.medium"
 }
