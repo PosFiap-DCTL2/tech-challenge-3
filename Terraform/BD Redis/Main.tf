c61f5133305901bcbc9323d070a8d272dcbe8a50
DaniloRferraz <danilor.ferraz@outlook.com>
2026-04-17 10:22:45 -0300

    Corrijido codigos vulneraveis

--- Terraform/BD Redis/Main.tf
+++ Terraform/BD Redis/Main.tf
@@ -1,9 +1,11 @@
 resource "aws_elasticache_cluster" "evaluation-service" {
   cluster_id           = "evaluation-service"
   engine               = "redis"
-  node_type            = "cache.m4.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.1"
   port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids   = [aws_security_group.redis_sg.id] 
 }
