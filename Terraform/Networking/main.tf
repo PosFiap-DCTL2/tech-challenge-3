5b379c96ac6e3ae288fa2231086555c2bdeaa3c2
DaniloRferraz <danilor.ferraz@outlook.com>
2026-05-21 01:17:08 +0000

    Alterando o security group redis

--- Terraform/Networking/main.tf
+++ Terraform/Networking/main.tf
@@ -136,7 +136,7 @@ resource "aws_security_group" "redis" {
     from_port       = 6379
     to_port         = 6379
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
