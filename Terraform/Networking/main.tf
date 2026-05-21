a082e19a1408f5b4c3ae35dd777cc4a53a5e8682
DaniloRferraz <danilor.ferraz@outlook.com>
2026-05-21 09:53:12 -0300

    Alterando o security group

--- Terraform/Networking/main.tf
+++ Terraform/Networking/main.tf
@@ -115,7 +115,7 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.eks.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
