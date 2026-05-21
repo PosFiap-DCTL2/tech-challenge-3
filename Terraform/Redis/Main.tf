a3b9a86c04f6940ca862e36f3d2a2ff9c0820ff3
DaniloRferraz <danilor.ferraz@outlook.com>
2026-05-21 00:03:00 +0000

    Redis

--- Terraform/Redis/Main.tf
+++ Terraform/Redis/Main.tf
@@ -1,13 +1,14 @@
 resource "aws_elasticache_replication_group" "evaluation-service" {
-  replication_group_id          = "evaluation-service"
-  replication_group_description = "Redis"
-  engine                        = "redis"
-  engine_version                = "7.1"
-  node_type                     = "cache.t3.micro"
-  number_cache_clusters         = 1
-  parameter_group_name          = "default.redis7"
-  port                          = 6379
+  replication_group_id   = "evaluation-service"
+  description            = "Redis para avaliação"
+  engine                 = "redis"
+  engine_version         = "7.1"
+  node_type              = "cache.t3.micro"
+  num_node_groups        = 1
+  replicas_per_node_group = 0
+  parameter_group_name   = "default.redis7"
+  port                   = 6379
 
-  subnet_group_name  = var.network.subnet_group_id
-  security_group_ids = [var.network.security_group_id]
+  subnet_group_name      = var.network.subnet_group_id
+  security_group_ids     = [var.network.security_group_id]
 }
